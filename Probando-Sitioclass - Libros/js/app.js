6301287ee2edcf5fa64d06d0597a54806a1e9ba3
MartinFernandez00 <danteprox20@gmail.com>
2023-09-06 15:09:24 -0300

    Mod 06.09

--- Probando-Sitioclass - Libros/js/app.js
+++ Probando-Sitioclass - Libros/js/app.js
@@ -1,7 +1,10 @@
-// console.log("El archivo Javascript ya esta importado");
+console.log("El archivo Javascript ya esta importado");
+
+const urlApi="https://ingenieria-2f72.restdb.io/rest/libros?apikey=a20e60d9b17f06daf23a2fa847eb7c7b7f763"
 
 const libros = {
     listarLibros: ()=>{
+        // Tomamos la referencia del contenedor donde se mostraran los libros
         const contenedor=document.getElementById("contenedorLibros");
         const books="<ul><li> Libro1 <li>Libro2 </ul>";
         contenedor.innerHTML=books;
