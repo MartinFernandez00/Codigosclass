e993268c27f1119b63873791c1cf9092a47ad45c
MartinFernandez00 <danteprox20@gmail.com>
2023-09-26 18:25:45 -0300

    ajustes pero aun sin resolver el error.

--- Probando-Sitioclass - Libros/js/app.js
+++ Probando-Sitioclass - Libros/js/app.js
@@ -4,10 +4,32 @@ const urlApi="https://ingenieria-2f72.restdb.io/rest/libros?apikey=64f8c70e68885
 
 const applibros = {
     listarLibros: ()=>{
+        //tomamos la referencia del contenedor donde se mostraran los libros.
+        const contenedor=document.getElementById("contenedorLibros");
+
+        let contenidoHTML = "";
+
+        fetch(urlApi)
+        .then(respuesta=>respuesta.json())
+        .then(libros=>{
+            console.log(libros);
+            for (const libro of libros) {
+                contenidoHTML+= `
+                <div>
+                    <img src="${libro.portada_url}" class="img-thumbnail"/>
+                    <details>
+                    <summary>${libro.nombre}</summary>
+                    ${libro.sinopsis}
+                    </details>
+                </div>
+                `
+            }
+            console.log(contenidoHTML)
+            contenedor.innerHTML=contenidoHTML;
+        })
     },
     agregarLibros: ()=>{
         alert("agregando un libro");
     }
 }
 applibros.listarLibros();
-applibros.agregarLibros();
--- Probando-Sitioclass - Libros/js/firebase.js
+++ Probando-Sitioclass - Libros/js/firebase.js
@@ -1,6 +1,7 @@
 //Import the functions you need from the SDKs you need
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-app.js";
+
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-auth.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
