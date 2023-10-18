b5f5c982eb32b4095d39e0825f6e78924a7d14f9
MartinFernadez00 <danteprox20@.com>
2023-10-18 20:31:28 -0300

    clase 18.10 Eliminar y Editar Libros

--- Probando-Sitioclass - Libros/js/app.js
+++ Probando-Sitioclass - Libros/js/app.js
@@ -27,7 +27,10 @@ const applibros = {
                         Páginas: ${libro.paginas}<br/>
                         Editorial: ${libro.editorial}
                     </details>
+
                     <a href="#" onclick="applibros.editarLibro('${libro._id}')">Editar</a>
+                    <a href="#" onclick="applibros.eliminarLibro('${libro._id}','${libro.nombre}')">Eliminar</a>
+                    
                 </div>
                 `
             }
@@ -35,8 +38,49 @@ const applibros = {
             contenedor.innerHTML=contenidoHTML;
         })
     },
-    agregarLibros: ()=>{
 
+eliminarLibro: (idAEliminar,nombreABorrar)=>{
+
+    Swal.fire({
+        title: `Seguro que quiere borrar el libro ${nombreABorrar}?`,
+        text: "No vas a poder cambiar esta operacion",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        cancelButtonText: 'Cancelar',
+        confirmButtonText: 'Si, quiero borrarlo'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            if (result.isConfirmed) {
+                const urlApi = `https://ingenieria-2f72.restdb.io/rest/libros/${idAEliminar}?apikey=64f8c70e688854d2c40bfe84`
+                //`https://pracprof2023-af4f.restdb.io/rest/peliculas/${idPeliculaBorrar}?apikey=6467b09a0b60fc42f4e197fa`
+                fetch(urlApi, {
+                    method: 'DELETE'
+                })
+                    .then(response => {
+                        console.log(response);
+                        return applibros.listarLibros();
+                    }).then(response => {
+                        Swal.fire(
+                            'Eliminado!',
+                            `El libro ${nombreABorrar} fue borrado .`,
+                            'satisfactoriamente'
+                        )
+                    });
+            }
+            Swal.fire(
+                'Borrado',
+                'Se borro el libro.',
+                'success'
+            )
+        }
+    })
+},
+
+    guardarLibros: ()=>{
+
+        const txtId=document.getElementById("txtId");
         const txtNombre=document.getElementById("txtNombre");
         const txtPaginas=document.getElementById("txtPaginas");
         const txtAutor=document.getElementById("txtAutor");
@@ -45,7 +89,21 @@ const applibros = {
         const txtSinopsis=document.getElementById("txtSinopsis");
         const txtGenero=document.getElementById("txtGenero");
 
-        const nuevoLibro = {
+        let urlApi='';
+        let metodoHttp='';
+
+        if (txtId.value === '') 
+        {
+            urlApi = "https://ingenieria-2f72.restdb.io/rest/libros?apikey=64f8c70e688854d2c40bfe84";
+            metodoHttp = 'POST';
+        }
+        else 
+        {
+            urlApi = `https://ingenieria-2f72.restdb.io/rest/libros/${txtId.value}?apikey=64f8c70e688854d2c40bfe84`;
+            metodoHttp = 'PUT';
+        }
+
+        const libroAGuardar = {
             "nombre":txtNombre.value,
             "paginas":txtPaginas.value,
             "autor":txtAutor.value,
@@ -54,13 +112,13 @@ const applibros = {
             "sinopsis":txtSinopsis.value,
             "genero":txtGenero.value
         };
-        console.log(nuevoLibro);
+        console.log(libroAGuardar);
         fetch(urlApi, {
-            method: "POST",
+            method: metodoHttp,
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(nuevoLibro)
+            body: JSON.stringify(libroAGuardar)
             })  
             .then(response => {
                 console.log(response);
@@ -69,11 +127,23 @@ const applibros = {
     },
     editarLibro:(idLibroAEditar)=>{
         const urlApi=`https://ingenieria-2f72.restdb.io/rest/libros/${idLibroAEditar}?apikey=64f8c70e688854d2c40bfe84`;
-        fetch(urlApi
-        ).then(res => res.json())
-            .then(libro => {
+
+        fetch(urlApi).then(res => res.json()).then(libro => {
+
+                document.getElementById("txtId").value=libro._id
                 document.getElementById("txtNombre").value=libro.nombre
-                console.log(libro);
+                document.getElementById("txtPaginas").value=libro.paginas
+                document.getElementById("txtAutor").value=libro.autor
+                document.getElementById("txtEditorial").value=libro.editorial
+                document.getElementById("txtPortadaUrl").value=libro.portada_url
+                document.getElementById("txtSinopsis").value=libro.sinopsis
+                document.getElementById("txtGenero").value=libro.genero
+
+                const VentanaEditar=document.getElementById('agregarEditarModal');
+                let modal = new bootstrap.Modal(VentanaEditar);
+                modal.show();
+// let = Crear Variable
+// modal.show = Muestra Siguiente
             });
         }
     }
